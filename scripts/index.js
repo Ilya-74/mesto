c1d4ce75bf994659fd88e98496423fdb5e8229d6
Ilya Zaytsev <zaitsev-i@74.ru>
2022-01-16 16:15:48 +0500

    сделать popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,13 @@
 const profileOpenPopupButton = document.querySelector('.profile__edit-button')
-console.log(profileOpenPopupButton)
+const popup = document.querySelector('.popup')
+const popupCloseButton = document.querySelector('.popup__close')
+const popupSeveButton = document.querySelector('.form__submit-btn')
+
+
+profileOpenPopupButton.addEventListener('click', function() {
+    popup.classList.add('popup__opened')
+})
+
+popupCloseButton.addEventListener('click', function() {
+    popup.classList.remove('popup__opened')
+})
