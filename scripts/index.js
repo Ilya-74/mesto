59f5fc8f1bc793c7151d4ab474436b78628ee603
Ilya Zaytsev <zaitsev-i@74.ru>
2022-01-16 21:16:30 +0500

    сделать redme

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 const profileOpenPopupButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const popupCloseButton = document.querySelector('.popup__close')
-const popupSeveButton = document.querySelector('.form__submit-btn')
+const popupSeveButton = document.querySelector('.popup__seve')
 
+function openPopup() {
+    popup.classList.add('popup_opened')
+}
 
-profileOpenPopupButton.addEventListener('click', function() {
-    popup.classList.add('popup__opened')
-})
+function closePopup() {
+    popup.classList.remove('popup_opened')
+}
 
-popupCloseButton.addEventListener('click', function() {
-    popup.classList.remove('popup__opened')
-})
+profileOpenPopupButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', closePopup)
