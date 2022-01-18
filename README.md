e88d9f0ab40a65942646da1b5dce6d8b9fc1e66c
Ilya Zaytsev <zaitsev-i@74.ru>
2022-01-18 23:03:44 +0500

    сделать проверку перед сдачей

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
-# Проект: Место
+<a id="anchor"></a>
+# Проект: [Место]()
 
 ### Обзор
 
-* Figma
-* Картинки
+* Profile
+* Cards-grid
+* Popup
 
-**Figma**
 
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
+__<h2 align="center">Mesto</h2>__
+_<p align="center">Здесь проект Mesto.</p>_
+![Картинка, Mesto](readme_assets/Mesto.png)
 
-**Картинки**
 
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
+__<h2 align="center">Profile</h2>__
+_<p align="center">В профиле, есть кнопка редактировать, ею вызывается окно формы.</p>_
+![Картинка колонка грид](readme_assets/Profile.png)
+
+
+__<h2 align="center">Cards-grid</h2>__
+_<p align="center">В Фото-грид размещен блок, с картинками, которые расположены в грид среде.</p>_
+![Картинка, колонка грид](readme_assets/Grid.png)
+
+
+__<h2 align="center">Popup</h2>__
+_<p align="center">Реализован сам popup с кнопкой закрыть, сохранить. И реализованы поля для ввода текста на языке js.</p>_
+![Картинка колонка грид](readme_assets/Popup.png)
 
 Удачи!
+[__<p align="end">Вверх</p>__](#anchor)
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
+//Положили в контенер и дали название.
+
 const profileOpenPopupButton = document.querySelector('.profile__edit-button')
+const profileName = document.querySelector('.profile__name')
+const profileBio = document.querySelector('.profile__bio')
 const popup = document.querySelector('.popup')
 const popupCloseButton = document.querySelector('.popup__close')
 const popupSeveButton = document.querySelector('.popup__seve')
+const nameInput = document.querySelector('.popup_input_name')
+const jobInput = document.querySelector('.popup_input_bio')
+
+//Определили функцию кнопки: открыть, закрыть popup.
 
 function openPopup() {
     popup.classList.add('popup_opened')
@@ -11,5 +19,24 @@ function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
+// запускаем слушателя функции подстановки переменных
+
 profileOpenPopupButton.addEventListener('click', openPopup)
 popupCloseButton.addEventListener('click', closePopup)
+
+//Напишем функцию для полей.
+
+profileOpenPopupButton.addEventListener('click', function () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileBio.textContent;    //Текст из html выводится в popup
+})
+
+popupSeveButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;    //Текст из popup  сохраняется в html
+  profileBio.textContent = jobInput.value;
+})
+
+popupSeveButton.addEventListener('click',function() {
+  closePopup(popupSeveButton)                              //Закрытие popup по кнопки 'сохранить'
+}); 
