58be553ffc449efe29595920282af4f12a92a944
Ilya Zaytsev <zaitsev-i@74.ru>
2022-01-27 23:11:47 +0500

    rev 2

--- scripts/index.js
+++ scripts/index.js
@@ -6,37 +6,28 @@ const profileBio = document.querySelector('.profile__bio')
 const popup = document.querySelector('.popup')
 const popupCloseButton = document.querySelector('.popup__close')
 const popupSeveButton = document.querySelector('.popup__seve')
-const nameInput = document.querySelector('.popup__input-name')
-const jobInput = document.querySelector('.popup__input-bio')
+const nameInput = document.querySelector('.popup__input_type_name')
+const jobInput = document.querySelector('.popup__input_type_bio')
 
 //Определили функцию кнопки: открыть, закрыть popup.
-
 function openPopup() {
     popup.classList.add('popup_opened')
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileBio.textContent; 
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
-// запускаем слушателя функции подстановки переменных
-
-profileOpenPopupButton.addEventListener('click', openPopup)
-popupCloseButton.addEventListener('click', closePopup)
-
 //Напишем функцию для полей.
-
-profileOpenPopupButton.addEventListener('click', function () {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileBio.textContent;    //Текст из html выводится в popup
-})
-
-popupSeveButton.addEventListener('click', function (evt) {
+popup.addEventListener('submit', function (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;    //Текст из popup  сохраняется в html
   profileBio.textContent = jobInput.value;
+  closePopup()
 })
 
-popupSeveButton.addEventListener('click',function() {
-  closePopup(popupSeveButton)                              //Закрытие popup по кнопки 'сохранить'
-}); 
+// запускаем слушателя функции подстановки переменных
+profileOpenPopupButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', closePopup)
