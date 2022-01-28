60100d2c244a26a16169201a7d7f66fa7bf721bb
Ilya Zaytsev <zaitsev-i@74.ru>
2022-01-28 20:04:27 +0500

    rev 3

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const popupCloseButton = document.querySelector('.popup__close')
 const popupSeveButton = document.querySelector('.popup__seve')
 const nameInput = document.querySelector('.popup__input_type_name')
 const jobInput = document.querySelector('.popup__input_type_bio')
-
+const popupForm = document.querySelector('.popup__form')
 //Определили функцию кнопки: открыть, закрыть popup.
 function openPopup() {
     popup.classList.add('popup_opened')
@@ -20,8 +20,7 @@ function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
-//Напишем функцию для полей.
-popup.addEventListener('submit', function (evt) {
+popupForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;    //Текст из popup  сохраняется в html
   profileBio.textContent = jobInput.value;
